cef1ee13aee42365a72800c0cc2a8f20ec52be7c
xiaotiancaipro <xiaotiancaipro@163.com>
2026-04-17 02:03:14 +0800

    Release: v0.2.1

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 func New() *cobra.Command {
 	c := &cobra.Command{
 		Short:   "nextunnel",
-		Version: "0.2.0",
+		Version: "v0.2.1",
 		Args:    cobra.ExactArgs(0),
 		Run:     func(cmd *cobra.Command, _ []string) { _ = cmd.Help() },
 	}
