49109e545b8614b47945d01f2168f2cbd8095fed
xiaotiancaipro <xiaotiancaipro@163.com>
2026-04-17 02:02:33 +0800

    Perf: Use regular expressions to standardize log output format

--- internal/utils/logger/formatter.go
+++ internal/utils/logger/formatter.go
@@ -13,12 +13,12 @@ type Formatter struct {
 }
 
 func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
-	re := regexp.MustCompile(`^.*github/nextunnel/`)
+	re := regexp.MustCompile(`^.*?(cmd/|internal/)`)
 	format := fmt.Sprintf(
 		"%s - %s - %s - %d - %s\n",
 		entry.Time.Format("2006-01-02 15:04:05"),
 		strings.ToUpper(entry.Level.String()),
-		re.ReplaceAllString(entry.Caller.File, ""),
+		re.ReplaceAllString(entry.Caller.File, "$1"),
 		entry.Caller.Line,
 		entry.Message,
 	)
