fcb9b59715dba1a5c07b7fa290df715eddddc3be
xiaotiancaipro <xiaotiancaipro@163.com>
2026-04-17 11:22:00 +0800

    Perf: Replace regular expressions with a predefined path array for improved performance

--- internal/utils/logger/formatter.go
+++ internal/utils/logger/formatter.go
@@ -2,25 +2,56 @@ package logger
 
 import (
 	"fmt"
-	"regexp"
+	"path/filepath"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
+
+var callerPathRoots = [...]string{
+	"cmd",
+	"internal",
+}
 
 type Formatter struct {
 	logrus.TextFormatter
 }
 
 func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
-	re := regexp.MustCompile(`^.*?(cmd/|internal/)`)
+	file := ""
+	line := 0
+	if entry.Caller != nil {
+		file = trimCallerPath(entry.Caller.File)
+		line = entry.Caller.Line
+	}
 	format := fmt.Sprintf(
 		"%s - %s - %s - %d - %s\n",
 		entry.Time.Format("2006-01-02 15:04:05"),
 		strings.ToUpper(entry.Level.String()),
-		re.ReplaceAllString(entry.Caller.File, "$1"),
-		entry.Caller.Line,
+		file,
+		line,
 		entry.Message,
 	)
 	return []byte(format), nil
+}
+
+func trimCallerPath(path string) string {
+	path = filepath.ToSlash(path)
+	for _, root := range callerPathRoots {
+		prefix := root + "/"
+		if strings.HasPrefix(path, prefix) {
+			return path
+		}
+	}
+	best := len(path)
+	for _, root := range callerPathRoots {
+		marker := "/" + root + "/"
+		if idx := strings.LastIndex(path, marker); idx >= 0 && idx+1 < best {
+			best = idx + 1
+		}
+	}
+	if best < len(path) {
+		return path[best:]
+	}
+	return path
 }
