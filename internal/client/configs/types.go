a59032351921646a4e1d9d0faa42315f4c8f77a3
xiaotiancaipro <xiaotiancaipro@163.com>
2026-07-23 11:22:29 +0800

    Refactor: [client] Delete the time zone configuration and use the system time zone

--- internal/client/configs/types.go
+++ internal/client/configs/types.go
@@ -5,10 +5,9 @@ import (
 )
 
 type Configs struct {
-	Server   *Server                 `toml:"server"`
-	Client   *Client                 `toml:"client"`
-	Cert     *Cert                   `toml:"cert"`
-	Logs     *sharedconfigs.Logs     `toml:"logs"`
-	Timezone *sharedconfigs.Timezone `toml:"timezone"`
-	Proxies  []Proxy                 `toml:"proxies"`
+	Server  *Server             `toml:"server"`
+	Client  *Client             `toml:"client"`
+	Cert    *Cert               `toml:"cert"`
+	Logs    *sharedconfigs.Logs `toml:"logs"`
+	Proxies []Proxy             `toml:"proxies"`
 }
